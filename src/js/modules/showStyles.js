ce952b9ae6266e2e8627bdb84dc0b157c83b4ffb
olga <yaolmi@yandex.ru>
2021-11-08 11:49:13 +0700

    edit styles for showStyles module

--- src/js/modules/showStyles.js
+++ src/js/modules/showStyles.js
@@ -21,14 +21,6 @@ function showStyles(triggerSelector, wrapperSelector) {
 		});
 	}
 
-	/*function showError() {
-		let errorMessage = document.createElement('div');
-		errorMessage.classList.add('error');
-		errorMessage.style.cssText = 'text-align:center;padding:30px;';
-		errorMessage.textContent = 'Что-то пошло не так... Попробуйте позже';
-		document.querySelector(wrapperSelector).append(errorMessage);
-	}*/
-
 	trigger.addEventListener('click', (e) => {
 		getResource('http://localhost:3000/styles')
 			.then(data => createCards(data))
